52bfc2c728bfc08ae409b5986fe1645def461927
mahdi abbasi <mahdiabbasi1100@gmail.com>
2020-04-03 13:51:35 +0430

    make root master and setting of page and crate frams of page

---
+++ main.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+#  settings
+root = Tk()
+root.mainloop()
+root.geometry('400x200')
+root.title('calculator')
+root.resizable(width=False, height=False)
+color = 'gray'
+root.configure(bg=color)
+
+#  frames
+top_first = Frame(root, width=400, height=50, bg='red')
+top_first.pack(side=TOP)
+
+top_second = Frame(root, width=400, height=50, bg='green')
+top_second.pack(side=TOP)
+
+top_third = Frame(root, width=400, height=50, bg='blue')
+top_third.pack(side=TOP)
+
+top_forth = Frame(root, width=400, height=50, bg='yellow')
+top_forth.pack(side=TOP)
